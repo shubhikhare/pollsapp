96d228fe29edc9098994be0e1b1cb76997b559e7
shubhikhare <shubhiverma712@gmail.com>
2016-01-30 15:07:49 +0530

    commit

--- pollsapp/urls.py
+++ pollsapp/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
